96faf3be322ea4833cf5cc37d6051d0e48ff5306
Szilágyi Dominik <dominik_szilagyi@yahoo.com>
2023-09-21 13:26:26 +0200

    Hibák kommentelése

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; // Hibás tömblétrehozás
+    std::cout << '1-100 ertekek duplazasa' // Nem szép kiírás
+    for (int i = 0;) // Hiányos for ciklus
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // Hiányos kiíratás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) // Hibás for ciklus szintaktika
     {
         atlag += b[i]
     }
